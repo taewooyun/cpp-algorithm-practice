eae157d93b88a36d371500369e53c14749d4b2ff
h3 <taewooyunn@gmail.com>
2025-06-05 01:05:24 +0900

    docs: programmers/templete 작성

---
+++ templete/programmers/main.cpp
@@ -0,0 +1,18 @@
+// 프로그래머스 [문제번호] - [문제 제목]
+// https://school.programmers.co.kr/learn/courses/30/lessons/문제번호
+// 간단한 문제 설명
+// 시간복잡도: O( )
+
+#include <iostream>
+
+using namespace std;
+
+
+
+int main() {
+    auto answer = solution();
+    
+    cout << answer << endl;
+
+    return 0;
+}
